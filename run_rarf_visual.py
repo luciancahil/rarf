361b5b73b24c19842a28be69083330b18be07d18
Roy He <royhe62@yahoo.ca>
2025-11-20 10:54:48 -0800

    concatenate the smiles

--- run_rarf_visual.py
+++ run_rarf_visual.py
@@ -5,14 +5,48 @@ from sklearn.model_selection import train_test_split
 from RaRFRegressor_shared_overlap_visual import RaRFRegressor
 from utils import smi2morgan
 
-# --- Load data ---
-df = pd.read_excel("Nature_SMILES.xlsx", sheet_name="df")
+def load_data():
+    xlsx = "Nature_SMILES.xlsx"
+    df = pd.read_excel(xlsx, sheet_name="df")
+
+    solvent = pd.read_excel(xlsx, sheet_name='solvent')
+    imine =  pd.read_excel(xlsx, sheet_name='imine')
+    nuc =  pd.read_excel(xlsx, sheet_name='nuc')
+    ligand = pd.read_excel(xlsx, sheet_name='ligand')
+
+    solvent = dict(zip(solvent['solvent'], solvent['SMILES_s']))
+    imine = dict(zip(imine['Imine'], imine['SMILES_i']))
+    nuc = dict(zip(nuc['Nucleophile'], nuc['SMILES_n']))
+    ligand = dict(zip(ligand['ligand'], ligand['SMILES_l']))
+
+    smiles_all = []
+
+    for index, row in df.iterrows():
+        print(index, row)
+        cur_smiles = solvent[row["Solvent"]] + imine[row["Imine"]] + nuc[row["Nucleophile"]] + ligand[row["Ligand"]]
+
+        smiles_all.append(cur_smiles)
+    
+    y = df["DDG"].values
+
+    return smiles_all, y
+
+smiles_all, y = load_data()
+
+"""# --- Load data ---
+xlsx = "Nature_SMILES.xlsx"
+df = pd.read_excel(xlsx, sheet_name="df")
+solvent = pd.read_excel(xlsx, sheet_name='solvent')
 y = df["DDG"].values
-smiles_all = df["SMILES_all"].values  # assume concatenated SMILES column
+# smiles_all = df["SMILES_all"].values  # assume concatenated SMILES column
+
+# temp fix
+smiles_all = solvent["SMILES_s"].values"""
 
 # Convert to Morgan fingerprints
 X = np.array([smi2morgan(s, nbits=2048) for s in smiles_all])
 X = np.vstack(X)
+breakpoint()
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=42)
@@ -28,3 +62,6 @@ print("Predictions shape:", out["preds"].shape)
 # Plot
 rarf.plot_overlap_map(X_train, X_test, out["selected"])
 print("Saved plot as overlap_map.png")
+
+
+## TODO: ASK ABOUT MASTERS TRANSFERS YOU DONUT
